fb970d08f443c6725d2e2a5fb59bedeaee620a94
= limbon <= soosle93@gmail.com>
2020-06-27 11:16:41 +0600

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import { useSelector } from 'react-redux';
 
 import { generateRoutes } from './utils/generateRoutes';
 import { router } from './router';
+import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => {
 	const message = useSelector((store: ApplicationStore) => store.message);
 
-	return <div>{generateRoutes(router)}</div>;
+	return (
+		<div style={{ height: '100%', flexDirection: 'column', display: 'flex' }}>
+			<div style={{ flex: 1 }}>{generateRoutes(router)}</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Route } from '../../utils/enums';
+
+import styles from './Footer.scss';
+
+const Footer: React.FC = () => {
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.legal}>
+				<span>© 2018 BaseballCloud</span>
+				<Link to={Route.TOS}>Terms of Service</Link>
+				<Link to={Route.Privacy}>Privacy Policy</Link>
+			</div>
+			<div className={styles.socials}>
+				<a href='https://baseballcloud.blog' target='_blank'>
+					Blog
+				</a>
+				<a href='http://twitter.com/baseballcloudus' target='_blank'>
+					Twitter
+				</a>
+				<a href='http://www.instagram.com/baseballcloudus/' target='_blank'>
+					Instagram
+				</a>
+				<a href='http://www.facebook.com/BaseballCloudUS/' target='_blank'>
+					Facebook
+				</a>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
