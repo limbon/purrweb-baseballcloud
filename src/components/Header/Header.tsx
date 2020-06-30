c3c19ae4e564979097232058b1e334ad7d677baf
limbon <soosle93@gmail.com>
2020-06-30 14:29:42 +0600

    Add header navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,40 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import { Route } from '../../utils/enums';
 
-import Logo from '../../assets/icons/logo.svg';
+import { selectProfile } from '../../utils/selectors';
 
-import styles from './Header.scss';
+import Logo from '../../assets/icons/logo.svg';
+import DefaultAvatar from '../../assets/images/default-avatar.png';
 import Dropdown from '../UI/Dropdown/Dropdown';
 
+import styles from './Header.scss';
+
 const Header: React.FC = () => {
+	const profile = useSelector(selectProfile);
+
 	return (
 		<header className={styles.header}>
 			<Link className={styles.homeLink} to={Route.Home}>
 				<Logo />
 			</Link>
-			<div style={{ marginLeft: 'auto' }}>
-				<Dropdown title='Dropdown' options={['Lorem ipsum dolor sit amet', 'Bar', 'Baz']} />
+			<div className={styles.navigation}>
+				<Link to={Route.Leaderboard}>Leaderboard</Link>
+				<Link to={Route.Network}>Network</Link>
+				{profile && (
+					<div className={styles.profile}>
+						<img src={profile?.avatar || DefaultAvatar} />
+						<Dropdown
+							title={`${profile?.first_name} ${profile?.last_name}`}
+							options={[
+								<Link to={Route.Profile}>My Profile</Link>,
+								<Link to='/logout'>Logout</Link>,
+							]}
+						/>
+					</div>
+				)}
 			</div>
 		</header>
 	);
