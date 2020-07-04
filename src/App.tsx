73de04b3c94adab36eca976e0968f053a52f2874
= limbon <= soosle93@gmail.com>
2020-07-04 16:03:42 +0600

    Fix fetching data before validating token

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,44 @@ import { useDispatch } from 'react-redux';
 
 import { router } from './router';
 
+import { useRoutine } from './hooks/useRoutine';
 import { generateRoutes } from './utils/generateRoutes';
 import { validateToken } from './ducks/user';
 import { fetchProfile } from './ducks/profile';
 
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Select from './components/UI/Select/Select';
 
 const App: React.FC = () => {
+	const [ready, setReady] = React.useState<boolean>(false);
 	const dispatch = useDispatch();
+	const [_, requestValidateToken] = useRoutine(
+		{ routine: validateToken, onSuccess: () => setReady(true) },
+		[],
+	);
 
 	React.useEffect(() => {
-		dispatch(validateToken());
-		dispatch(fetchProfile());
+		requestValidateToken();
 	}, []);
 
-	return (
-		<div style={{ height: '100%', flexDirection: 'column', display: 'flex' }}>
-			<Header />
+	React.useEffect(() => {
+		if (ready) {
+			dispatch(fetchProfile());
+		}
+	}, [ready]);
 
-			<div style={{ flex: 1, overflow: 'hidden' }}>{generateRoutes(router)}</div>
-			<Footer />
-		</div>
-	);
+	if (ready) {
+		return (
+			<div style={{ height: '100%', flexDirection: 'column', display: 'flex' }}>
+				<Header />
+
+				<div style={{ flex: 1, overflow: 'hidden' }}>{generateRoutes(router)}</div>
+				<Footer />
+			</div>
+		);
+	} else {
+		return <h1>Loading...</h1>;
+	}
 };
 
 export default App;
